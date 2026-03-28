9b4789a364338863aee45fd99a9b3b394ef5e65c
rohan-005 <rohandhanerwal@gmail.com>
2026-03-29 01:43:36 +0530

    url_fix

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,7 @@ const app = express();
 const httpServer = createServer(app);
 const io = new Server(httpServer, {
   cors: {
-    origin: process.env.CLIENT_URL || "http://localhost:5173",
+    origin: process.env.CLIENT_URL || "https://lvl-0.vercel.app/",
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true
   }
