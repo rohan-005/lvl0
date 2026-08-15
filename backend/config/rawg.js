892091cff26ed31d40c3a9bbd457ac0a49bb3316
frosthowl <rohandhanerwal@gmail.com>
2026-08-15 21:27:26 +0530

    phase -2 : complete microservice refactoring and cleanup

--- backend/config/rawg.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-  baseURL: process.env.RAWG_BASE_URL,
-  apiKey: process.env.RAWG_API_KEY,
-};
--- backend/utils/generateToken.js
+++
@@ -1,9 +0,0 @@
-const jwt = require('jsonwebtoken');
-
-const generateToken = (id, expiresIn = '30d') => {
-  return jwt.sign({ id }, process.env.JWT_SECRET, {
-    expiresIn: expiresIn,
-  });
-};
-
-module.exports = generateToken;
